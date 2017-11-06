e06fe33a8adf6b6b5e006bd75c941db08009f4b9
Jeep liberty light <1138724604@qq.com>
2017-11-06 10:26:37 +0800

    Update index(1).js

--- vue仿新闻网站/src/components/loading/index(1).js
+++ vue仿新闻网站/src/components/loading/index(1).js
@@ -1,7 +1,7 @@
 const LoadingComponent = require('./Loading.vue')
 const loading = {
   install: function(Vue) {
-    Vue.component('loading', LoadingComponent)
+    Vue.component('loading', LoadingComponent)   //给vue添加了自身的属性
   }
 }
 module.exports = loading
