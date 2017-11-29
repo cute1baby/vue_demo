ac7d179ce5b2d33a89045531d177a9b8bd77d470
Jeep liberty light <1138724604@qq.com>
2017-11-29 11:53:03 +0800

    Update main.js

--- vue框架/5custom自定义组件/src/main.js
+++ vue框架/5custom自定义组件/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import Loading from './components/loading'
 
-Vue.use(Loading);
+Vue.use(Loading);   //注意：在此处直接使用Loading相当于全局覆盖了。
 new Vue({
   el: '#app',
   render: h => h(App)
